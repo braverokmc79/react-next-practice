e2dd96d84c93b3c6ee598793056e3659de7cdf78
braverokmc79 <braverokmc79@gmail.com>
2022-09-16 12:31:56 +0900

    first commit

---
+++ components/Nav.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        Home
+                    </Link>
+                </li>
+
+                <li>
+                    <Link href="/photos">
+                        photos
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ pages/photos.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function photos() {
+    return (
+        <div>My Photos</div>
+    )
+}
+
+export default photos
